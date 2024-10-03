a82b6916c65121d61ff7f647456e5ba8c47a34fb
julia prado <108271775+jul1a150@users.noreply.github.com>
2024-10-03 13:48:50 -0300

    tt

--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = ' '
 
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
